70c34e541a60adf2dddfa59584071303f676b676
hanifrasendra <133474723+hanifrasendra@users.noreply.github.com>
2026-04-14 03:34:20 +0700

    ""

--- api/conf.php
+++ api/conf.php
@@ -1,18 +1,14 @@
-<?php 
-   $conn = mysqli_init();
-    mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-    mysqli_real_connect(
-        $conn,
-        $host,
-        $username,
-        $pass,
-        $nama_db,
-        3306,
-        NULL,
-        MYSQLI_CLIENT_SSL
-    );
+<?php
+$host     = getenv('DATABASE_HOST');
+$username = getenv('DATABASE_USER');
+$pass     = getenv('DATABASE_PASS');
+$nama_db  = getenv('DATABASE_DB');
+$port     = getenv('DATABASE_PORT'); // tambah port
 
-    if (mysqli_connect_error()) {
-        die(json_encode(["status" => "gagal", "message" => "Koneksi DB gagal"]));
-    }
-?>
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_real_connect($conn, $host, $username, $pass, $nama_db, $port, NULL, MYSQLI_CLIENT_SSL);
+
+if (mysqli_connect_error()) {
+    die(json_encode(["status" => "gagal", "message" => "Koneksi DB gagal: " . mysqli_connect_error()]));
+}
