b02c6339078f62baf4db10475c037dade5d8ec19
hanifrasendra <133474723+hanifrasendra@users.noreply.github.com>
2026-04-14 18:38:17 +0700

    "adding delete"

--- api/delete.php
+++ api/delete.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: * ");
 header("Access-Control-Allow-Methods: GET, POST");
 header("Content-Type: application/json");
 
