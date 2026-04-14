cb528f6d15ba4f29dfd6e82314a8ca0749922a3c
hanifrasendra <133474723+hanifrasendra@users.noreply.github.com>
2026-04-14 18:33:12 +0700

    "adding delete"

---
+++ api/delete.php
@@ -0,0 +1,38 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Content-Type: application/json");
+
+include 'conf.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+$input = json_decode(file_get_contents("php://input"), true);
+$id = $_GET['id'];
+
+if (!$id) {
+    echo json_encode([
+        "status" => "error",
+        "message" => "ID tidak ditemukan"
+    ]);
+    exit();
+}
+
+$query = mysqli_query($conn, "DELETE * FROM pengajuan where id = '$id'");
+$result = mysqli_query($conn, $query);
+
+if($result){
+    echo json_encode([
+        "status" => "success",
+        "message" => "Data berhasil dihapus"
+    ]);
+} else {
+    echo json_encode([
+        "status" => "error",
+        "message" => "Gagal menghapus data"
+    ]);
+}
+?>
